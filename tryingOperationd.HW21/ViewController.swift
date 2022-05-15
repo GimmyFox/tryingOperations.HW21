3591f1c8d84c02dc18a91c33fc6035153f84ccd1
GimmyFox <mguzeev99@gmail.com>
2022-05-15 18:07:51 +0300

    Initial Commit

---
+++ tryingOperationd.HW21/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tryingOperationd.HW21
+//
+//  Created by Maksim Guzeev on 15.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
